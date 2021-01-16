8312cfb802968041a6c0bffa31ec20744a4d84a0
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-01-16 09:56:55 +0530

    chore : added comments for better understanding of code

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ let stockDetailsItem = document.querySelectorAll(".weight-bold");
 let api_key = "K7GCCMCOXNSL32G9";
 let stockDetails = [];
 
+//generating url to fetch based on the company selected
 function generateURL(companyName){
     // return `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${companyName}&apikey=${api_key}`;
     return "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=demo";
 }
 
-
+//calculating profit/loss and displaying the output in the output divs
 function calcProfitLoss(costPrice, currentCostPrice){
     let profit = currentCostPrice - costPrice;
     let profitPercent = profit * 100/ costPrice;
@@ -37,12 +38,22 @@ function calcProfitLoss(costPrice, currentCostPrice){
 
 }
 
+
+//filling the stock details of the selected company
 function fillStockDetails(items){
     for(let i=0; i<items.length; i++){
         stockDetailsItem[i].innerHTML = items[i];
     }
 }
 
+
+//error handler
+function handleError(error){
+    console.log("Error:", error)
+    alert("There is something wrong with the server, please try again in sometime")
+}
+
+//called when the calculate button is clicked.
 function clickHandler(){
     let noOfShares = parseInt(shareInput.value);
     let priceOfShare = parseInt(priceInput.value);
@@ -55,6 +66,7 @@ function clickHandler(){
     .then(json => {
         console.log(json);
         currentPrice = parseInt(json["Global Quote"]["05. price"]);
+        //populating the stockdetails array so as to pass it to the fillStockDetails function
         stockDetails = [json["Global Quote"]["02. open"],json["Global Quote"]["08. previous close"],json["Global Quote"]["05. price"],json["Global Quote"]["03. high"],json["Global Quote"]["04. low"],json["Global Quote"]["06. volume"]];
         fillStockDetails(stockDetails);
         console.log(stockDetails);
@@ -62,7 +74,8 @@ function clickHandler(){
         console.log(json["Global Quote"]["05. price"]);
     })
     .then( run => {
-        calcProfitLoss(costPrice, currentPrice*noOfShares)});
+        calcProfitLoss(costPrice, currentPrice*noOfShares)})
+    .catch(handleError)
 }
 
 
